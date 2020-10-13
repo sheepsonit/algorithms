fc95f7fd84a6482ac07a36af3a31694ed908c760
Svetlana Buyukova <buyukova@mail.ru>
2020-10-13 11:48:53 +0300

    added DFS algorithm

---
+++ src/com/sheepsonit/DepthFirstSearch.java
@@ -0,0 +1,40 @@
+package com.sheepsonit;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+public class DepthFirstSearch {
+    public static List<String> getFileTree(String root) {
+        File rootDir = new File(root);
+        Queue<File> allFiles = new LinkedList<>();
+        List<String> listFilePaths = new ArrayList<>();
+        if (rootDir.isDirectory())
+            allFiles.add(rootDir);
+        while (!allFiles.isEmpty()) {
+            File item = allFiles.poll();
+            if (item.isDirectory()) {
+                File[] files = item.listFiles();
+                if (files != null) {
+                    for (File file : files) {
+                        if (file.isDirectory()) {
+                            File[] childFiles = file.listFiles();
+                            if (childFiles != null)
+                                allFiles.addAll(Arrays.asList(childFiles));
+                        } else {
+                            listFilePaths.add(file.getAbsolutePath());
+                        }
+                    }
+                }
+            } else
+                listFilePaths.add(item.getAbsolutePath());
+        }
+
+        return listFilePaths;
+    }
+
+    public static void main (String[]args) throws IOException {
+        List<String> lists = getFileTree("D:/test");
+        lists.forEach(System.out::println);
+    }
+}
